ec859c43745c2a4311ea94fc6e452ced94264d88
Abolfazl <abolfazl.dmg@gmail.com>
2023-07-07 13:34:31 +0330

    Update test.py

--- vocal_interaction/wake_word/test.py
+++ vocal_interaction/wake_word/test.py
@@ -1,1 +1,14 @@
+# Write your tests here
 
+def test1(param1, param2):
+  pass
+
+def test2(param1, param2):
+  pass
+
+
+p1 = 'Example parameter 1'
+p2 = 'Example parameter 2'
+
+test1(p1, p2)
+test2(p1, p2)
